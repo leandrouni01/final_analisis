9d1ee59d723c59d4b97b3c45e46d98d9dd59a0e0
LeandroMurgo <leandrouni01@gmail.com>
2019-03-18 20:31:57 -0300

    Se arreglo localidad

---
+++ src/vista/js/funcionesLocalidad.js
@@ -0,0 +1,250 @@
+$(function () {
+    var TallerAvanzada = {};
+
+    (function (app) {
+        app.init = function () {
+            app.buscarLocalidades();
+            app.buscarPaises();
+            app.bindings();
+        };
+
+        app.bindings = function () {
+            $("#agregarLocalidad").on('click', function () {
+                $("#tituloModal").html("Agregar localidad");
+                $("#id").val(0);
+                $("#accion").html("Guardar");
+                $("#modalLocalidad").modal({show: true});
+            });
+            
+            $("#accion").on('click',function(){
+               if($("#accion").html()=="Guardar"){
+                   if($("#id").val()==0){
+                       app.guardarLocalidad();
+                   }else{
+                       app.editarLocalidad();
+                   }
+               }else{
+                   app.eliminarLocalidad();
+               } 
+            });
+            
+            $("#cuerpoTablaLocalidades").on('click','.editar',function(){
+               $("#tituloModal").html("Editar localidad");
+               $("#id").val($(this).attr("data-id_localidad"))
+               //alert($(this).parent().parent().children().first().next().attr("data-id_pais"));
+               $("#selectPais").find("option[value='"+$(this).parent().parent().children().first().next().attr("data-id_pais")+"']").prop("selected","true");
+                app.buscarProvincia($(this).parent().parent().children().first().html());
+               var html= "<option value='"+$(this).parent().parent().children().first().next().next().attr("data-id_provincia")+"'>"+$(this).parent().parent().children().first().next().next().html()+"</option>";
+               $("#selectProvincia").prepend(html);
+               $("#accion").html("Guardar");
+               $("#modalLocalidad").modal({show:true});
+            });
+            
+            $("#cuerpoTablaLocalidades").on('click','.eliminar',function(){
+               $("#tituloModal").html("¿Está seguro que desea eliminar esta localidad?");
+               $("#id").val($(this).attr("data-id_localidad"));
+               $("#selectPais").find("option[value='"+$(this).parent().parent().children().first().next().attr("data-id_pais")+"']").prop("selected","true");
+               var html= "<option value='"+$(this).parent().parent().children().first().next().next().attr("data-id_provincia")+"'>"+$(this).parent().parent().children().first().next().next().html()+"</option>";
+               $("#selectProvincia").prepend(html);
+               $("#nombreLocalidad").val($(this).parent().parent().children().first().html());
+               $("#fieldsetLocalidad").attr("disabled","true");
+               $("#accion").html("Eliminar");
+               $("#modalLocalidad").modal({show:true});
+            });
+            
+            $("#cuerpoTablaLocalidades").on('click','.ver',function(){
+               $("#tituloModal").html("Ver localidad");
+               $("#id").val($(this).attr("data-id_localidad"));
+               $("#selectPais").find("option[value='"+$(this).parent().parent().children().first().next().attr("data-id_pais")+"']").prop("selected","true");
+               var html= "<option value='"+$(this).parent().parent().children().first().next().next().attr("data-id_provincia")+"'>"+$(this).parent().parent().children().first().next().next().html()+"</option>";
+               $("#selectProvincia").prepend(html);
+               $("#nombreLocalidad").val($(this).parent().parent().children().first().html());
+               $("#fieldsetLocalidad").attr("disabled","true");
+               $("#accion").hide();
+               $("#modalLocalidad").modal({show:true});
+            });
+            
+            $("#selectPais").on("change",function(){
+                app.buscarProvincia();
+            });
+            
+            $("#modalLocalidad").on('hide.bs.modal', function () {
+                app.limpiarModal();
+            });
+        };
+        
+        app.guardarLocalidad= function(){
+            var url= "../../controlador/ruteador/Ruteador.php?accion=agregar&Formulario=Localidad";
+            var datosEnviar= $("#formLocalidad").serialize();
+            $.ajax({
+               url:url,
+               method: 'POST',
+               dataType: 'json',
+               data: datosEnviar,
+               success: function(datosRecibidos){
+                   app.actualizarTabla(datosRecibidos,$("#id").val());
+                   $("#modalLocalidad").modal('hide');
+               },
+               error: function(datosRecibidos){
+                   alert("Error al guardar Localidad");
+                   alert(datosRecibidos);
+               }
+            });
+        };
+        
+        app.editarLocalidad = function(){
+            var url = "../../controlador/ruteador/Ruteador.php?accion=modificar&Formulario=Localidad";
+            var datosEnviar = $("#formLocalidad").serialize();
+            $.ajax({
+                url: url,
+                method: 'POST',
+                dataType: 'json',
+                data: datosEnviar,
+                success: function(datosRecibidos){
+                    app.actualizarTabla(datosRecibidos,$("#id").val());
+                    $("#modalLocalidad").modal("hide");
+                },
+                error: function(datosRecibidos){
+                    alert("error al editar localidad");
+                    alert(datosRecibidos);
+                }
+            });
+        };
+        
+        app.actualizarTabla = function(localidad,id){
+          var html = "";
+          if(id==0){
+              html = "<tr>\n\
+                          <td>"+localidad.nombre_localidad+"</td>\n\
+                          <td data-id_pais='"+localidad.fk_pais+"'>"+localidad.nombre_pais+"</td>\n\
+                          <td data-id_provincia='"+localidad.fk_provincia+"'>"+localidad.nombre_provincia+"</td>\n\
+                          <td><a class='ver' data-id_localidad='"+localidad.id_localidad+"'><span class='glyphicon glyphicon-info-sign'></span>Ver</a><a class='eliminar' data-id_localidad='"+localidad.id_localidad+"'><span class='glyphicon glyphicon-remove'></span>Eliminar</a><a class='editar' data-id_localidad='"+localidad.id_localidad+"'><span class='glyphicon glyphicon-pencil'></span>Editar</a></td>\n\
+                      </tr>";
+                $("#cuerpoTablaLocalidades").append(html);
+          }else{
+              var fila= $("#cuerpoTablaLocalidades").find("a[data-id_localidad='"+id+"']").parent().parent();
+              html = "<td>"+$("#nombreLocalidad").val()+"</td>\n\
+                      <td data-id_pais='"+$("#selectPais").find(":selected").val()+"'>"+$("#selectPais").find(":selected").html()+"</td>\n\
+                      <td data-id_provincia='"+$("#selectProvincia").find(":selected").val()+"'>"+$("#selectProvincia").find(":selected").html()+"</td>\n\
+                      <td><a class='ver' data-id_localidad='"+id+"'><span class='glyphicon glyphicon-info-sign'></span>Ver</a><a class='eliminar' data-id_localidad='"+id+"'><span class='glyphicon glyphicon-remove'></span>Eliminar</a><a class='editar' data-id_localidad='"+id+"'><span class='glyphicon glyphicon-pencil'></span>Editar</a></td>";
+                fila.html(html);
+          }
+        };
+        
+        app.eliminarLocalidad= function(){
+            var url= "../../controlador/ruteador/Ruteador.php?accion=eliminar&Formulario=Localidad";
+            var datosEnviar= $("#formLocalidad").serialize();
+            $.ajax({
+               url: url,
+               method: 'POST',
+               dataType: 'json',
+               data: datosEnviar,
+               success: function(){
+                   app.eliminarFila($("#id").val());
+                   $("#modalLocalidad").modal("hide");
+               },
+               error : function(){
+                   
+               }
+            });
+        };
+        
+        app.eliminarFila= function(id){
+            $("#cuerpoTablaLocalidades").find("a[data-id_localidad='"+id+"']").parent().parent().remove();
+        };
+
+        app.buscarLocalidades = function () {
+            var url = "../../controlador/ruteador/Ruteador.php?accion=listar&Formulario=Localidad";
+            $.ajax({
+                url: url,
+                method: 'GET',
+                dataType: 'json',
+                success: function (datosRecibidos) {
+                    app.rellenarTabla(datosRecibidos);
+                },
+                error: function (datosRecibidos) {
+                    alert("Error al buscar localidades");
+                    alert(datosRecibidos);
+                }
+            });
+        };
+
+        app.rellenarTabla = function (datosLocalidad) {
+            var html = "";
+            $.each(datosLocalidad, function (clave, localidad) {
+                html += "<tr>\n\
+                            <td>" + localidad.nombre_localidad + "</td>\n\
+                            <td data-id_pais='" + localidad.fk_pais + "'>" + localidad.nombre_pais + "</td>\n\
+                            <td data-id_provincia='" + localidad.fk_provincia + "'>" + localidad.nombre_provincia + "</td>\n\
+                            <td><a class='ver' data-id_localidad='" + localidad.id_localidad + "'><span class='glyphicon glyphicon-info-sign'></span>Ver</a><a class='eliminar' data-id_localidad='" + localidad.id_localidad + "'><span class='glyphicon glyphicon-remove'></span>Eliminar</a><a class='editar' data-id_localidad='" + localidad.id_localidad + "'><span class='glyphicon glyphicon-pencil'></span>Editar</a></td>\n\
+                        </tr>";
+            });
+            $("#cuerpoTablaLocalidades").html(html);
+        };
+
+        app.buscarPaises = function () {
+            var url = "../../controlador/ruteador/Ruteador.php?accion=listar&Formulario=Pais";
+            $.ajax({
+                url: url,
+                method: 'GET',
+                dataType: 'json',
+                success: function (datosRecibidos) {
+                    app.rellenarPaises(datosRecibidos);
+                },
+                error: function (datosRecibidos) {
+                    alert("Error al buscar paises");
+                    alert(datosRecibidos);
+                }
+            });
+        };
+
+        app.rellenarPaises = function (datosPaises) {
+            var html = "<option value=0>Seleccione un pais</option>";
+            $.each(datosPaises, function (clave, pais) {
+                html += "<option value='" + pais.id_pais + "'>" + pais.nombre_pais + "</option>";
+            });
+            $("#selectPais").html(html);
+        };
+        
+        app.buscarProvincia= function(datos){
+            var url= "../../controlador/ruteador/Ruteador.php?accion=listarProvincias&Formulario=Localidad";
+            var datosEnviar= $("#formLocalidad").serialize();
+            $.ajax({
+                url: url,
+                method: 'POST',
+                dataType: 'json',
+                data: datosEnviar,
+                success: function(datosRecibidos){
+                    app.rellenarProvincia(datosRecibidos,datos);
+                },
+                error: function(datosRecibidos){
+                    alert("Error al buscar provincias");
+                    alert(datosRecibidos);
+                }
+            });
+        };
+        
+        app.rellenarProvincia= function(datosProvincia,datos){
+            var html= "<option value=0>Seleccione una provincia</option>";
+            $.each(datosProvincia,function(clave,provincia){
+                html += "<option value='"+provincia.id_provincia+"'>"+provincia.nombre_provincia+"</option>";
+            });
+            if($("#tituloModal").html()=="Editar localidad"&&$("#nombreLocalidad").val()==""){
+                $("#selectProvincia").append(html);
+                $("#nombreLocalidad").val(datos);
+            }else{
+                $("#selectProvincia").html(html);
+            }
+        };
+        
+        app.limpiarModal= function(){
+            $("#selectPais").find("option[value=0]").prop("selected","true");
+            $("#selectProvincia").html("");
+            $("#nombreLocalidad").val("");
+            $("#fieldsetLocalidad").removeAttr("disabled");
+            $("#accion").show();
+        };
+
+        app.init();
+    })(TallerAvanzada);
+});
