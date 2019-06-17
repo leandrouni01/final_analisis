91ad49182f1312a71e2f77e339d33558d4473625
Luciano <lucianooo222@gmail.com>
2019-06-17 20:08:23 -0300

    Se agregaron metodos buscar Duracion , Materias y Cursos

--- src/controlador/controladoresEspecificos/ControladorEspacioCurricular.php
+++ src/controlador/controladoresEspecificos/ControladorEspacioCurricular.php
@@ -64,8 +64,49 @@ class ControladorEspacioCurricular extends ControladorGeneral
 
             $this->refControladorPersistencia->ejecutarSentencia(DbSentencias::ACTUALIZAR_ESPACIO_CURRICULAR, $parametros);
             $this->refControladorPersistencia->confirmarTransaccion();
-        } catch (Execption $e) {
+        } catch (Exeption $e) {
             $this->refControladorPersistencia->rollBackTransaccion();
+            echo "Error :" . $e->getMessage();
+        }
+    }
+
+    public function buscarDuracion($datos){
+        //recibe id
+        try {
+            $parametros = array("id_plan" => $datos["id_plan"]);
+            $resultado = $this->refControladorPersistencia->ejecutarSentencia(DbSentencias::BUSCAR_MATERIAS_EC ,$parametros);
+            $fila = $resultado->fetch();
+
+            return $fila;
+        } catch (Exception $e){
+            echo "Error :" . $e->getMessage();
+        }
+    }
+
+    public function buscarMaterias($datos){
+        //recibe idplan , anio
+        try {
+            $parametros = array("id_materia" => $datos["id_materia"] , 
+            "anio" => $datos["anio"]);
+            $resultado = $this->refControladorPersistencia->ejecutarSentencia(DbSentencias::BUSCAR_MATERIAS_EC, $parametros);
+            $fila = $resultado->fetchAll(PDO::FETCH_ASSOC);
+
+            return $fila;
+        } catch(Exception $e){
+            echo "Error :" . $e->getMessage(); 
+        }
+    }
+
+    public function buscarCursos($datos){
+        //id_sede , anios
+        try {
+            $parametros = array("fk_sede" => $datos["id_sede"],
+            "anio_curso" => $datos["anio"]);
+            $resultado = $this->refControladorPersistencia->ejecutarSentencia(DbSentencias::BUSCAR_CURSOS_EC, $parametros);
+            $fila = $resultado->fetchAll(PDO::FETCH_ASSOC);
+
+            return $fila;
+        } catch(Execption $e){
             echo "Error :" . $e->getMessage();
         }
     }
