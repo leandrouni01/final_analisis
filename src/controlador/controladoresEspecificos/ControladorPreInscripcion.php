db9f1af0e047eee5af0c01c29874ffe32e533a32
LeandroMurgo <leandrouni01@gmail.com>
2019-07-28 20:15:07 -0300

    Se completo controlador de preinscripcion

--- src/controlador/controladoresEspecificos/ControladorPreInscripcion.php
+++ src/controlador/controladoresEspecificos/ControladorPreInscripcion.php
@@ -1,8 +1,128 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+require_once 'ControladorGeneral.php';
 
+class ControladorPreInscripcion extends ControladorGeneral{
+    
+    function __construct() {
+        parent::__construct();
+    }
+    
+    public function agregar($datos) {
+        try{
+            $this->refControladorPersistencia->inciciarTransaccion();
+            $parametros= [
+                "fk_plan"=>$datos["fk_plan"],
+                "fk_sede"=>$datos["fk_sede"],
+                "fk_alumno"=>$datos["fk_alumno"],
+                "anio"=>$datos["ciclo_lectivo_horario"],
+                "documentacion"=>$datos["documentacion"]
+            ];
+            $this->refControladorPersistencia->ejecutarSentencia(DbSentencias::INSERTAR_PRE_ISNCRIPCION,$parametros);
+            $this->refControladorPersistencia->confirmarTransaccion();
+            return $this->buscarUltimaPreinscripcion();
+        } catch (Exception $e) {
+            $this->refControladorPersistencia->rollBackTransaccion();
+            echo "Failed: " . $e->getMessage();
+        }
+    }
+    
+    public function buscarAlumno($datos){
+        try{
+            $parametros=[
+              "legajo"=>$datos["legajo"]
+            ];
+            $resultado= $this->refControladorPersistencia->ejecutarSentencia(DbSentencias::BUSCAR_ALUMNO,$parametros);
+            $array= $resultado->fetch(PDO::FETCH_ASSOC);
+            return $array;
+        } catch (Exception $e) {
+            echo "Failed: " . $e->getMessage();
+        }
+    }
+    
+    public function buscarSedes($datos){
+        try{
+            $parametros= [
+                "fk_plan"=>$datos["fk_plan"]
+            ];
+            $resultado=$this->refControladorPersistencia->ejecutarSentencia(BUSCAR_SEDES,$parametros);
+            $array= $resultado->fetchAll(PDO::FETCH_ASSOC);
+            return $array;
+        } catch (Exception $e) {
+            echo "Failed: " . $e->getMessage();
+        }
+    }
+    
+    public function buscarPlanes() {
+        try {
+            $resultado = $this->refControladorPersistencia->ejecutarSentencia(BUSCAR_PLANES_DE_ESTUDIO);
+            $array = $resultado->fetchAll(PDO::FETCH_ASSOC);
+            return $array;
+        } catch (Exception $e) {
+            echo "Failed: " . $e->getMessage();
+        }
+    }
+
+    public function buscarUltimaPreinscripcion(){
+        try{
+            $resultado= $this->refControladorPersistencia->ejecutarSentencia(DbSentencias::BUSCAR_ULTIMA_PRE_INSCRIPCION);
+            $fila= $resultado->fetch(PDO::FETCH_ASSOC);
+            return $fila;
+        } catch (Exception $e) {
+            echo "Error :" . $e->getMessage();
+        }
+    }
+
+    public function buscar($datos) {
+        
+    }
+
+    public function eliminar($datos) {
+        try {
+            $this->refControladorPersistencia->inciciarTransaccion();
+            $parametros = [
+                "fk_plan" => $datos["fk_plan"],
+                "fk_sede" => $datos["fk_sede"],
+                "fk_alumno" => $datos["fk_alumno"],
+                "anio" => $datos["ciclo_lectivo_horario"],
+                "documentacion" => $datos["documentacion"],
+                "id_preinscripcion" => $datos["id_pre_inscripcion"]
+            ];
+            $this->refControladorPersistencia->ejecutarSentencia(DbSentencias::ELIMINAR_PRE_INSCRIPCION, $parametros);
+            $this->refControladorPersistencia->confirmarTransaccion();
+        } catch (Exception $e) {
+            $this->refControladorPersistencia->rollBackTransaccion();
+            echo "Failed: " . $e->getMessage();
+        }
+    }
+
+    public function listar($datos) {
+        try{
+           $resultado= $this->refControladorPersistencia->ejecutarSentencia(DbSentencias::LISTAR_PRE_INSCRIPCIONES);
+           $array= $resultado->fetchAll(PDO::FETCH_ASSOC);
+           return $array;
+        } catch (Exception $e) {
+            echo "Error :" . $e->getMessage();
+        }
+    }
+
+    public function modificar($datos) {
+        try {
+            $this->refControladorPersistencia->inciciarTransaccion();
+            $parametros = [
+                "fk_plan" => $datos["fk_plan"],
+                "fk_sede" => $datos["fk_sede"],
+                "fk_alumno" => $datos["fk_alumno"],
+                "anio" => $datos["ciclo_lectivo_horario"],
+                "documentacion" => $datos["documentacion"],
+                "id_preinscripcion" => $datos["id_pre_inscripcion"]
+            ];
+            $this->refControladorPersistencia->ejecutarSentencia(DbSentencias::ACTUALIZAR_PRE_INSCRIPCION, $parametros);
+            $this->refControladorPersistencia->confirmarTransaccion();
+            } catch (Exception $e) {
+            $this->refControladorPersistencia->rollBackTransaccion();
+            echo "Failed: " . $e->getMessage();
+        }
+    }
+
+}
